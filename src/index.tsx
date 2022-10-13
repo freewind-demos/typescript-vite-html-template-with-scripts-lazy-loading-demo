7934dc442a525cefbb732a8dcdcf2c398c3cefdf
Freewind <nowindlee@gmail.com>
2022-10-13 18:06:41 +0800

    typescript-vite-html-template-with-scripts-lazy-loading-demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 const temp = document.createElement('template') as HTMLTemplateElement;
-const div = document.createElement('dev');
+const div = document.createElement('div');
 div.innerHTML = `
-<div>Inside template</div>
-<script>alert('Thanks!')</script>
+<div>Inside dynamic template</div>
+<script>alert('Inside dynamic template!')</script>
 `
 temp.content.appendChild(div)
 
+document.body.appendChild(temp);
 document.body.appendChild(document.importNode(temp.content, true));
+document.body.appendChild(document.importNode((document.getElementById('template1') as HTMLTemplateElement).content, true));
 
